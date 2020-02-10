6322a35f28d0c8a538e2a58f69ff203c0604980a
Andrew K <29694231+AWK8923@users.noreply.github.com>
2020-02-10 15:17:07 -0500

    Pretty colors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,6 +16,12 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -32,6 +38,7 @@ public class Robot extends TimedRobot {
   private final Compressor comp = new Compressor(1);
   private final DoubleSolenoid sol1 = new DoubleSolenoid(1, 0, 1);
   private final XboxController cont = new XboxController(2);
+  private final ColorSensorV3 color = new ColorSensorV3(I2C.Port.kOnboard);
   //DifferentialDrive myDrive;
  
   
@@ -95,23 +102,52 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-  /*  //m_robotDrive.arcadeDrive(m_stick.getY()`, m_stick.getX()); 
-    if(isOperatorControl()&& isEnabled()){
-    m_robotDrive.tankDrive((m_stick.getY()*-1), (x_stick.getY()*-1));//test
-    Timer.delay(0.01); 
-  }*/
+    //m_robotDrive.arcadeDrive(m_stick.getY()`, m_stick.getX()); 
+  
+  //Movement Controls
+  //m_robotDrive.tankDrive((m_stick.getY()*-1), (x_stick.getY()*-1));//test
+  //Timer.delay(0.01); 
+  
+  //Gamepad Solenoid Control
   if(cont.getAButton()) {
-        sol1.set(Value.kForward);
+    sol1.set(Value.kForward);
     }
   else if(cont.getBButton()) {
-      sol1.set(Value.kReverse);
+    sol1.set(Value.kReverse);
   }
-}
 
+  //Color Sensor Values
+  Color detectedColor = color.getColor();
+  double IR = color.getIR();
+  double[] colorArray = {detectedColor.red, detectedColor.green, detectedColor.blue};
+
+  System.out.println(colorArray[0] + "    " + colorArray[1] + "    " + colorArray[2]);
+  //.3, .5, .1 Yellow
+  //.5, .3, .1 Red
+  //.1, .5, .2 Green
+  //.1, .4, .4 Blue
+  if(colorArray[0] > .3 && colorArray[0] < .4 && colorArray[1] > .5 && colorArray[1] < .6 && colorArray[2] > .1 && colorArray[2] < .2){
+    sol1.set(Value.kForward);
+    System.out.println("Yellow");
+  }
+  if(colorArray[0] > .5 && colorArray[0] < .6 && colorArray[1] > .3 && colorArray[1] < .4 && colorArray[2] > .1 && colorArray[2] < .2){
+    sol1.set(Value.kForward);
+    System.out.println("Red");
+  }
+  if(colorArray[0] > .1 && colorArray[0] < .2 && colorArray[1] > .5 && colorArray[1] < .6 && colorArray[2] > .2 && colorArray[2] < .3){
+    sol1.set(Value.kReverse);
+    System.out.println("Green");
+  }
+  if(colorArray[0] > .1 && colorArray[0] < .2 && colorArray[1] > .4 && colorArray[1] < .5 && colorArray[2] > .4 && colorArray[2] < .5){
+    sol1.set(Value.kReverse);
+    System.out.println("Blue");
+  }
+} 
   /**
    * This function is called periodically during test mode.
    */
   @Override
   public void testPeriodic() {
+    
   }
 }
